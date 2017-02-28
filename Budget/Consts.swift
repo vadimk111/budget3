6430fa3d417176144e2683af6fd08835385a0ac0
vadimkononov <vadim.kononov@kaltura.com>
2017-02-28 14:03:25 +0200

    add colors

--- Budget/Consts.swift
+++ Budget/Consts.swift
@@ -12,7 +12,8 @@ let colors: [UIColor] = [UIColor(red: 103 / 255, green: 171 / 255, blue: 87 / 25
                          UIColor(red: 252 / 255, green: 208 / 255, blue: 13 / 255, alpha: 1),
                          UIColor(red: 167 / 255, green: 117 / 255, blue: 242 / 255, alpha: 1),
                          UIColor(red: 89 / 255, green: 186 / 255, blue: 243 / 255, alpha: 1),
-                         UIColor(red: 212 / 255, green: 0 / 255, blue: 28 / 255, alpha: 1)]
+                         UIColor(red: 66 / 255, green: 66 / 255, blue: 66 / 255, alpha: 1),
+                         UIColor(red: 255 / 255, green: 124 / 255, blue: 16 / 255, alpha: 1)]
 
 let months: [String] = ["January", "February", "March", "April", "May", "June",
                         "July", "August", "September", "October", "November", "December"]
