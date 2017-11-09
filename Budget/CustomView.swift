3878f6e43e7bf62e09b8625486dedebf772f824e
Vadim Kononov <vadim.kononov@kaltura.com>
2017-11-09 15:56:57 +0200

    fix constraints warning

--- Budget/CustomView.swift
+++ Budget/CustomView.swift
@@ -24,11 +24,11 @@ class CustomView: UIView {
     
     func loadXib() {
         view = Bundle.main.loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?[0] as! UIView
+        addSubview(view)
         view.translatesAutoresizingMaskIntoConstraints = false
         addConstraint(NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0))
         addConstraint(NSLayoutConstraint(item: view, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0))
         addConstraint(NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0))
         addConstraint(NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: 0))
-        addSubview(view)
     }
 }
